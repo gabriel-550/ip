4d3db66a03eb7d117892ad20b8772abd84432ab6
gabriel-550 <glim5891@gmail.com>
2026-01-26 23:33:12 +0800

    Add, List input tasks

--- src/main/java/Enzo.java
+++ src/main/java/Enzo.java
@@ -4,17 +4,28 @@ public class Enzo {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(" Hello! I'm Enzo");
         System.out.println(" What can I do for you?");
 
         while (true) {
             String input = in.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println(" Bye. I’ll be right here when you need me again!");
                 break;
-            }
-            System.out.println(" " + input);
-        }
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
 
+                System.out.println(" added: " + input);
+            }
+        }
     }
 }
