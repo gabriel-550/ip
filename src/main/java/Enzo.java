db6464c66b00afd1c53aacb2c75eb8b8db05577e
gabriel-550 <glim5891@gmail.com>
2026-01-26 18:53:47 +0800

    Echo imputs

--- src/main/java/Enzo.java
+++ src/main/java/Enzo.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Enzo {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         System.out.println(" Hello! I'm Enzo");
         System.out.println(" What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(" Bye. I’ll be right here when you need me again!");
+                break;
+            }
+            System.out.println(" " + input);
+        }
+
     }
 }
