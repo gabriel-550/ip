8bea801a77c243aa95ded6add1e03922f8f85d5e
gabriel-550 <glim5891@gmail.com>
2026-02-06 15:21:49 +0800

    Improve Code Quality

--- src/main/java/Enzo.java
+++ src/main/java/Enzo.java
@@ -1,72 +1,66 @@
 import java.util.Scanner;
 
 public class Enzo {
+    public static final int MAX_TASKS = 100;
+    public static final int MARK_PREFIX_LENGTH = 5;
+    public static final int UNMARK_PREFIX_LENGTH = 7;
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+    public static final int TODO_PREFIX_LENGTH = 5;
+    public static final int DEADLINE_PREFIX_LENGTH = 9;
+    public static final int EVENT_PREFIX_LENGTH = 6;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        printWelcome();
 
-        Task[] tasks = new Task[100];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
-
-        System.out.println(" Hello! I'm Enzo");
-        System.out.println(" What can I do for you?");
 
         while (true) {
             String input = in.nextLine();
 
             if (input.equals("bye")) {
-                System.out.println(" Bye. I’ll be right here when you need me again!");
+                printGoodbye();
                 break;
 
             } else if (input.equals("list")) {
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + "." + tasks[i]);
-                }
+                handleList(tasks, taskCount);
 
             } else if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
+                int index = Integer.parseInt(input.substring(MARK_PREFIX_LENGTH)) - DISPLAYED_INDEX_OFFSET;
                 tasks[index].mark();
 
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(" " + tasks[index]);
 
             } else if (input.startsWith("unmark ")) {
-                int index = Integer.parseInt(input.substring(7)) - 1;
+                int index = Integer.parseInt(input.substring(UNMARK_PREFIX_LENGTH)) - DISPLAYED_INDEX_OFFSET;
                 tasks[index].unmark();
 
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(" " + tasks[index]);
 
             } else if (input.startsWith("todo ")) {
-                String description = input.substring(5);
+                String description = input.substring(TODO_PREFIX_LENGTH);
                 tasks[taskCount++] = new Todo(description);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(" " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                printTaskAdded(tasks, taskCount);
 
             } else if (input.startsWith("deadline ")) {
-                String[] parts = input.substring(9).split(" /by ");
+                String[] parts = input.substring(DEADLINE_PREFIX_LENGTH).split(" /by ");
                 String description = parts[0];
                 String by = parts[1];
 
                 tasks[taskCount++] = new Deadline(description, by);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(" " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                printTaskAdded(tasks, taskCount);
 
             } else if (input.startsWith("event ")) {
-                String[] parts = input.substring(6).split(" /from | /to ");
+                String[] parts = input.substring(EVENT_PREFIX_LENGTH).split(" /from | /to ");
                 String description = parts[0];
                 String from = parts[1];
                 String to = parts[2];
 
                 tasks[taskCount++] = new Event(description, from, to);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(" " + tasks[taskCount - 1]);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                printTaskAdded(tasks, taskCount);
 
             } else {
                 tasks[taskCount] = new Task(input);
@@ -75,5 +69,27 @@ public class Enzo {
                 System.out.println(" added: " + input);
             }
         }
+    }
+
+    private static void printWelcome() {
+        System.out.println(" Hello! I'm Enzo");
+        System.out.println(" What can I do for you?");
+    }
+
+    private static void printGoodbye() {
+        System.out.println(" Bye. I’ll be right here when you need me again!");
+    }
+
+    private static void handleList(Task[] tasks, int taskCount) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(" " + (i + 1) + "." + tasks[i]);
+        }
+    }
+
+    private static void printTaskAdded(Task[] tasks, int taskCount) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(" " + tasks[taskCount - 1]);
+        System.out.println("Now you have " + taskCount + " tasks in the list.");
     }
 }
