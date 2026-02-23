85584d131895587ed1d230bf74feafcddb0612dc
gabriel-550 <glim5891@gmail.com>
2026-02-24 00:02:59 +0800

    Extract TaskList and Ui classes

--- src/main/java/Enzo.java
+++ src/main/java/Enzo.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Enzo {
     public static final int MARK_PREFIX_LENGTH = 5;
@@ -12,15 +11,16 @@ public class Enzo {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        printWelcome();
+        Ui ui = new Ui();
+        ui.showWelcome();
 
         Storage storage = new Storage("./data/enzo.txt");
-        ArrayList<Task> tasks = new ArrayList<>();
+        TaskList tasks = new TaskList();
 
         try {
-            tasks = storage.load();
+            tasks = new TaskList(storage.load());
         } catch (EnzoException e) {
-            System.out.println(e.getMessage());
+            ui.showLoadingError();
         }
 
         while (true) {
@@ -28,98 +28,75 @@ public class Enzo {
                 String input = in.nextLine();
 
                 if (input.equals("bye")) {
-                    printGoodbye();
+                    ui.showGoodbye();
                     break;
 
                 } else if (input.equals("list")) {
-                    handleList(tasks);
+                    ui.showList(tasks);
 
                 } else if (input.startsWith("mark")) {
-                    handleMark(input, tasks);
-                    storage.save(tasks);
+                    handleMark(input, tasks, ui);
+                    storage.save(tasks.getTasks());
 
                 } else if (input.startsWith("unmark")) {
-                    handleUnmark(input, tasks);
-                    storage.save(tasks);
+                    handleUnmark(input, tasks, ui);
+                    storage.save(tasks.getTasks());
 
                 } else if (input.startsWith("delete")) {
-                    handleDelete(input, tasks);
-                    storage.save(tasks);
+                    handleDelete(input, tasks, ui);
+                    storage.save(tasks.getTasks());
 
                 } else if (input.startsWith("todo")) {
-                    handleTodo(input, tasks);
-                    storage.save(tasks);
+                    handleTodo(input, tasks, ui);
+                    storage.save(tasks.getTasks());
 
                 } else if (input.startsWith("deadline")) {
-                    handleDeadline(input, tasks);
-                    storage.save(tasks);
+                    handleDeadline(input, tasks, ui);
+                    storage.save(tasks.getTasks());
 
                 } else if (input.startsWith("event")) {
-                    handleEvent(input, tasks);
-                    storage.save(tasks);
+                    handleEvent(input, tasks, ui);
+                    storage.save(tasks.getTasks());
 
                 } else {
                     throw new EnzoException("Sorry, I don't understand this command");
                 }
             } catch (EnzoException e) {
-                System.out.println(e.getMessage());
+                ui.showError(e.getMessage());
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Hey! Task number is out of range.");
+                ui.showError("Hey! Task number is out of range.");
             }
         }
     }
 
-    private static void printWelcome() {
-        System.out.println(" Hello! I'm Enzo");
-        System.out.println(" How can I help you today?");
-    }
-
-    private static void printGoodbye() {
-        System.out.println(" Bye. I'll be right here when you need me again!");
-    }
-
-    private static void handleList(ArrayList<Task> tasks) {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println(" " + (i + 1) + "." + tasks.get(i));
-        }
-    }
-
-    private static void handleMark(String input, ArrayList<Task> tasks) throws EnzoException {
+    private static void handleMark(String input, TaskList tasks, Ui ui) throws EnzoException {
         if (input.length() <= MARK_PREFIX_LENGTH) {
             throw new EnzoException("Hey! Please specify which task to mark.");
         }
 
         int index = parseTaskIndex(input, MARK_PREFIX_LENGTH, tasks.size());
         tasks.get(index).mark();
-
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(" " + tasks.get(index));
+        ui.showMarked(tasks.get(index));
     }
 
-    private static void handleUnmark(String input, ArrayList<Task> tasks) throws EnzoException {
+    private static void handleUnmark(String input, TaskList tasks, Ui ui) throws EnzoException {
         if (input.length() <= UNMARK_PREFIX_LENGTH) {
             throw new EnzoException("Hey! Please specify which task to unmark.");
         }
 
         int index = parseTaskIndex(input, UNMARK_PREFIX_LENGTH, tasks.size());
         tasks.get(index).unmark();
-
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(" " + tasks.get(index));
+        ui.showUnmarked(tasks.get(index));
     }
 
-    private static void handleDelete(String input, ArrayList<Task> tasks) throws EnzoException {
+    private static void handleDelete(String input, TaskList tasks, Ui ui) throws EnzoException {
         if (input.length() <= DELETE_PREFIX_LENGTH) {
             throw new EnzoException("Hey! Please specify which task to delete.");
         }
 
         int index = parseTaskIndex(input, DELETE_PREFIX_LENGTH, tasks.size());
         Task removedTask = tasks.remove(index);
-
-        System.out.println("Noted. I've removed this task:");
-        System.out.println("  " + removedTask);
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        ui.showDeleted(removedTask, tasks.size());
     }
 
     private static int parseTaskIndex(String input, int prefixLength, int taskCount) throws EnzoException {
@@ -138,7 +115,7 @@ public class Enzo {
         }
     }
 
-    private static void handleTodo(String input, ArrayList<Task> tasks) throws EnzoException {
+    private static void handleTodo(String input, TaskList tasks, Ui ui) throws EnzoException {
         if (input.length() <= TODO_PREFIX_LENGTH) {
             throw new EnzoException("Hey! A todo needs a description and cannot be left empty");
         }
@@ -149,10 +126,10 @@ public class Enzo {
         }
 
         tasks.add(new Todo(description));
-        printTaskAdded(tasks);
+        ui.showTaskAdded(tasks);
     }
 
-    private static void handleDeadline(String input, ArrayList<Task> tasks) throws EnzoException {
+    private static void handleDeadline(String input, TaskList tasks, Ui ui) throws EnzoException {
         if (!input.contains(" /by ")) {
             throw new EnzoException("Hey! Please specify the deadline with /by");
         }
@@ -166,10 +143,10 @@ public class Enzo {
         }
 
         tasks.add(new Deadline(description, by));
-        printTaskAdded(tasks);
+        ui.showTaskAdded(tasks);
     }
 
-    private static void handleEvent(String input, ArrayList<Task> tasks) throws EnzoException {
+    private static void handleEvent(String input, TaskList tasks, Ui ui) throws EnzoException {
         if (!input.contains(" /from ") || !input.contains(" /to ")) {
             throw new EnzoException("Hey! Events need /from and /to details");
         }
@@ -184,12 +161,6 @@ public class Enzo {
         }
 
         tasks.add(new Event(description, from, to));
-        printTaskAdded(tasks);
-    }
-
-    private static void printTaskAdded(ArrayList<Task> tasks) {
-        System.out.println("Got it. I've added this task:");
-        System.out.println(" " + tasks.get(tasks.size() - 1));
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        ui.showTaskAdded(tasks);
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,47 @@
+public class Ui {
+    public void showWelcome() {
+        System.out.println(" Hello! I'm Enzo");
+        System.out.println(" How can I help you today?");
+    }
+
+    public void showGoodbye() {
+        System.out.println(" Bye. I'll be right here when you need me again!");
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    }
+
+    public void showLoadingError() {
+        System.out.println("Error loading tasks from file.");
+    }
+
+    public void showList(TaskList tasks) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(" " + (i + 1) + "." + tasks.get(i));
+        }
+    }
+
+    public void showMarked(Task task) {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(" " + task);
+    }
+
+    public void showUnmarked(Task task) {
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(" " + task);
+    }
+
+    public void showDeleted(Task task, int remainingCount) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + remainingCount + " tasks in the list.");
+    }
+
+    public void showTaskAdded(TaskList tasks) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(" " + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+    }
+}
